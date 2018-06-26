75885560682d4cbf59348fb5b29361290e1e3598
ddowns15 <37954010+ddowns15@users.noreply.github.com>
2018-06-25 18:16:17 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',  //where do we start
+  output: {
+    path: path.join(__dirname, 'public'), //cant use shortcut; must do full path
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/, //regex check if the file getting loading ends with .js
+      exclude: /node_modules/ //when you see a node file (not ours) dont include it
+    }, {
+      test: /\.s?css$/, //regex for any file ending in .css or .scss
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }
+};
