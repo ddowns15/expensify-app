229f3ef4828c8defcabe1dbcf714f8475689ed23
ddowns15 <37954010+ddowns15@users.noreply.github.com>
2018-07-05 13:45:38 -0700

    Lecture 90 done

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/playground/destructuring.js',  //where do we start
+  entry: './src/playground/redux-101.js',  //where do we start
   output: {
     path: path.join(__dirname, 'public'), //cant use shortcut; must do full path
     filename: 'bundle.js'
