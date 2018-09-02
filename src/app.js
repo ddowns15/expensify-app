40fb7bad0502fd10e6cd8c660dc15abf48a9df1c
ddowns15 <37954010+ddowns15@users.noreply.github.com>
2018-09-02 11:28:05 -0700

    Lecture 99 - Up to challenge
    up to 10:20 in video

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 
+const store = configureStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
